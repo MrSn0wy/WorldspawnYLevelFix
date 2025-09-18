a38d68f5bc323e3448c1af411bfd4c16e027a53a
MrSnowy <snow@mrsnowy.dev>
2025-09-18 17:47:06 +0200

    Forgot to bump version

--- common/src/main/java/dev/mrsnowy/worldspawn_ylevel_fix/Constants.java
+++ common/src/main/java/dev/mrsnowy/worldspawn_ylevel_fix/Constants.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class Constants {
     public static final String MOD_ID = "worldspawn_ylevel_fix";
     public static final String MOD_NAME = "Worldspawn Y-Level Fix";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 }
